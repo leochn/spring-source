b7ce57689ae8d22ad508f788b62aca5a2d5ebce8
leo <appchn@163.com>
2018-08-06 17:10:15 +0800

    first commit

---
+++ src/main/java/com/vnext/config/PersonConfig.java
@@ -0,0 +1,9 @@
+package com.vnext.config;
+
+/**
+ * @author leo
+ * @since 2018/8/6 17:09
+ */
+public class PersonConfig {
+
+}
---
+++ src/main/java/com/vnext/pojo/Person.java
@@ -0,0 +1,9 @@
+package com.vnext.pojo;
+
+/**
+ * @author leo
+ * @since 2018/8/6 17:09
+ */
+public class Person {
+
+}
